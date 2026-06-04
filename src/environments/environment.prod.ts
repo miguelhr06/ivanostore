8d63cb4b996408310ece614068d0bbe987ad7f84
miguelhr06 <miguelherrerarojas06@gmail.com>
2026-06-04 09:51:01 -0500

    Forzar creación de carpeta environments

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,14 @@
+export const environment = {
+  production: true,
+  supabaseUrl: '',
+  supabaseKey: '',
+  culqiPK: '',
+  firebase: {
+    apiKey: '',
+    authDomain: '',
+    projectId: '',
+    storageBucket: '',
+    messagingSenderId: '',
+    appId: ''
+  }
+};
